1f403a4b22517883cf57a56a99596bab2864d936
Alexandra Palermo <alexmpalermo@gmail.com>
2019-04-18 19:57:21 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,15 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-      @songs = Artist.find(params[:artist_id]).songs
-    else 
-    redirect_to artists_path, alert: "Artist not found"
-  end
+      @artist = Artist.find_by(id: params[:artist_id])
+      if @artist.nil?
+        redirect_to artists_path, alert: "Artist not found"
+      else
+        @songs = @artist.songs
+      end
+    else
+      @songs = Song.all
+    end
 end 
 
   def show
